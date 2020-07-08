b64dc3ccd1e3bd820e472a6fcf2158ddacd4e937
Deepak <17deepakrv5@gmail.com>
2020-07-08 22:17:50 +0530

    Palindrome number without converitng into string

--- PalindromeNumber.js
+++ PalindromeNumber.js
@@ -21,3 +21,18 @@ var isPalindrome = function(x) {
 console.log(isPalindrome(12345));
 console.log(isPalindrome(45654));
 console.log(isPalindrome(-123454321));
+
+//Coud you solve it without converting the integer to a string?
+var isItPalindrome = function(x) {
+    orinum = x;
+    revNumber = 0;
+    while (x > 0) {
+      revNumber = (revNumber * 10) + (x % 10);
+      x = Math.floor(x / 10);
+    }
+      return(orinum == revNumber ? true : false)
+  }
+
+console.log(isItPalindrome(12345));
+console.log(isItPalindrome(45654));
+console.log(isItPalindrome(-123454321));
